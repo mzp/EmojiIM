bfc6d2a7fd3e010fc460a5df16a099711b3f77f7
mzp <mzpppp@gmail.com>
2017-09-11 07:50:07 +0900

    Initial Commit

---
+++ EmojiIM/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  EmojiIM
+//
+//  Created by mzp on 2017/09/11.
+//  Copyright © 2017 mzp. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
