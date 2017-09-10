3c8ede5779a2971256afa1e16ef01597bd706d6f
mzp <mzpppp@gmail.com>
2017-09-11 08:05:41 +0900

    :lipstick: fix by following swiftlint

--- Sources/AppDelegate.swift
+++ Sources/AppDelegate.swift
@@ -9,10 +9,9 @@
 import Cocoa
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+internal class AppDelegate: NSObject, NSApplicationDelegate {
 
-    @IBOutlet weak var window: NSWindow!
-
+    @IBOutlet private weak var window: NSWindow!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
@@ -21,7 +20,4 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
 }
-
