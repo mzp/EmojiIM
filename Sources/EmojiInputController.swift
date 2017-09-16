7e9992835e04424d33c14bb3cdef7f6f8a9ae9f5
mzp <mzpppp@gmail.com>
2017-09-16 09:19:13 +0900

    :fountain_pen: emoji only input method

--- Sources/EmojiInputController.swift
+++ Sources/EmojiInputController.swift
@@ -12,7 +12,12 @@ import InputMethodKit
 open class EmojiInputController: IMKInputController {
     // swiftlint:disable:next implicitly_unwrapped_optional
     open override func inputText(_ string: String!, client sender: Any!) -> Bool {
-        NSLog("EmojiIM: \(string)")
-        return false
+        NSLog("EmojiIM: \(string): \(sender)")
+
+        guard let client = sender as? IMKTextInput else {
+            return false
+        }
+        client.insertText("🍣", replacementRange: NSRange(location: NSNotFound, length: NSNotFound))
+        return true
     }
 }
