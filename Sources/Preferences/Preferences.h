870b4159718af94177f211aad8443a52017e1727
mzp <mzpppp@gmail.com>
2017-11-04 10:22:46 +0900

    :wrench: add preferences target

---
+++ Sources/Preferences/Preferences.h
@@ -0,0 +1,15 @@
+//
+//  Preferences.h
+//  Preferences
+//
+//  Created by mzp on 2017/11/04.
+//  Copyright © 2017 mzp. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface Preferences : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
