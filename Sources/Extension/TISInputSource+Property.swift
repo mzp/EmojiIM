efba8a411d87449ca0ce0c8f504003b842e6e9de
mzp <mzpppp@gmail.com>
2017-11-17 21:19:21 +0900

    :sparkles: add extension for TISInputSource

---
+++ Sources/Extension/TISInputSource+Property.swift
@@ -0,0 +1,41 @@
+//
+//  TISInputSource+Property.swift
+//  Preferences
+//
+//  Created by mzp on 2017/11/13.
+//  Copyright © 2017 mzp. All rights reserved.
+//
+
+extension TISInputSource {
+    var localizedName: String {
+        return unsafeBitCast(
+            TISGetInputSourceProperty(self, kTISPropertyLocalizedName),
+            to: NSString.self) as String
+    }
+
+    var inputSourceID: String {
+        return unsafeBitCast(
+            TISGetInputSourceProperty(self, kTISPropertyInputSourceID),
+            to: NSString.self) as String
+    }
+
+    class func keyboardLayouts() -> [TISInputSource]? {
+        let conditions = CFDictionaryCreateMutable(nil, 2, nil, nil)
+        CFDictionaryAddValue(conditions,
+                             unsafeBitCast(kTISPropertyInputSourceType, to: UnsafeRawPointer.self),
+                             unsafeBitCast(kTISTypeKeyboardLayout, to: UnsafeRawPointer.self))
+        CFDictionaryAddValue(conditions,
+                             unsafeBitCast(kTISPropertyInputSourceIsASCIICapable, to: UnsafeRawPointer.self),
+                             unsafeBitCast(kCFBooleanTrue, to: UnsafeRawPointer.self))
+
+        guard let array = TISCreateInputSourceList(conditions, true) else {
+            return nil
+        }
+        guard let keyboards = array.takeRetainedValue() as? [TISInputSource] else {
+            return nil
+        }
+        return keyboards.sorted {
+            $0.localizedName < $1.localizedName
+        }
+    }
+}
